4be18dae761f17cf2437376a4bab9c599584d1e7
R Whitten <rex@skymodules.io>
2024-12-15 03:40:11 +0000

    feat(repository-terraform)

--- .github/terraform/variables.tf
+++ .github/terraform/variables.tf
@@ -1,3 +1,8 @@
+variable "GITHUB_OWNER" {
+  description = "The owner of this repository"
+  type        = string
+}
+
 variable "GITHUB_REPOSITORY" {
   description = "The name of this repository"
   type        = string
