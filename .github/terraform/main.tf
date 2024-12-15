d8ab1e552774a4be84eb447a2579fee84c76c130
R Whitten <rex@skymodules.io>
2024-12-15 03:51:51 +0000

    feat(repository-terraform)

--- .github/terraform/main.tf
+++ .github/terraform/main.tf
@@ -4,13 +4,27 @@ data "github_repository" "this" {
 }
 
 resource "github_actions_variable" "owner" {
-  repository       = data.github_repository.this.name
-  variable_name    = "organization_name"
-  value            = var.GITHUB_OWNER
+  repository    = data.github_repository.this.name
+  variable_name = "organization_name"
+  value         = var.GITHUB_OWNER
 }
 
 resource "github_actions_variable" "project" {
-  repository       = data.github_repository.this.name
-  variable_name    = "organization_name"
-  value            = var.GITHUB_REPOSITORY
+  repository    = data.github_repository.this.name
+  variable_name = "organization_name"
+  value         = var.GITHUB_REPOSITORY
+}
+
+
+
+# environments
+resource "github_repository_environment" "pr" {
+  count               = var.pr_environment.enabled ? 1 : 0
+  repository          = data.github_repository.this.name
+  environment         = var.pr_environment.name
+  prevent_self_review = true
+  deployment_branch_policy {
+    protected_branches     = false
+    custom_branch_policies = true
+  }
 }
--- .github/terraform/variables.tf
+++ .github/terraform/variables.tf
@@ -7,3 +7,48 @@ variable "GITHUB_REPOSITORY" {
   description = "The name of this repository"
   type        = string
 }
+
+variable "pr_environment" {
+  description = <<DESCRIPTION
+    The name of the environment to deploy the pull request to.
+  DESCRIPTION
+  type = object({
+    name    = string
+    enabled = bool
+  })
+
+  default = {
+    name    = "pr"
+    enabled = true
+  }
+}
+
+variable "live_environment" {
+  description = <<DESCRIPTION
+    Live Environment
+  DESCRIPTION
+  type = object({
+    name    = string
+    enabled = bool
+  })
+
+  default = {
+    name    = "live"
+    enabled = false
+  }
+}
+
+variable "test_environment" {
+  description = <<DESCRIPTION
+    Test environment
+  DESCRIPTION
+  type = object({
+    name    = string
+    enabled = bool
+  })
+
+  default = {
+    name    = "live"
+    enabled = false
+  }
+}
