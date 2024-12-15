a261ad9f86f49cb7e11193a9ed719cbeb18bc6cc
R Whitten <rex@skymodules.io>
2024-12-15 03:41:42 +0000

    feat(repository-terraform)

--- .github/terraform/main.tf
+++ .github/terraform/main.tf
@@ -3,3 +3,14 @@ data "github_repository" "this" {
   full_name = var.GITHUB_REPOSITORY
 }
 
+resource "github_actions_variable" "owner" {
+  repository       = data.github_repository.this.name
+  variable_name    = "organization_name"
+  value            = var.GITHUB_OWNER
+}
+
+resource "github_actions_variable" "project" {
+  repository       = data.github_repository.this.name
+  variable_name    = "organization_name"
+  value            = var.GITHUB_REPOSITORY
+}
