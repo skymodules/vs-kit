1680b4cca07b9d615a7ee9dc46358a6cd580585f
R Whitten <rex@skymodules.io>
2025-01-11 12:31:11 -0500

    fix(tf): update check.yml

--- .github/terraform/labels.tf
+++ .github/terraform/labels.tf
@@ -1,7 +1,7 @@
 
 # [GitHub Labels](https://docs.github.com/en/rest/reference/issues#labels)
 resource "github_issue_label" "urgent" {
-  repository = "test-repo"
+  repository = data.github_repository.this.name
   name       = "Urgent"
   color      = "FF0000"
 }
--- .github/terraform/teams.tf
+++
@@ -1,6 +0,0 @@
-# GitHub Teams
-resource "github_team" "main_team" {
-  name        = "some-team"
-  description = "Some cool team"
-  privacy     = "closed"
-}
