c5ac7eff30419bb876af31b3f5374b49e89d227d
R Whitten <rex@skymodules.io>
2025-01-11 12:34:00 -0500

    fix(ci): removed labels.tf

--- .github/terraform/labels.tf
+++
@@ -1,7 +0,0 @@
-
-# [GitHub Labels](https://docs.github.com/en/rest/reference/issues#labels)
-resource "github_issue_label" "urgent" {
-  repository = data.github_repository.this.name
-  name       = "Urgent"
-  color      = "FF0000"
-}
